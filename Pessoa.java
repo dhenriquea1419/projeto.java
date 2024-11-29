ef4ba063f44b554d6445f04566916a70e26a9006
danielhenrique <165221001+dhenriquea1419@users.noreply.github.com>
2024-11-28 22:48:25 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,27 @@
+package LojaVirtual.modelo;
+
+public abstract class Pessoa {
+    protected int id;
+    protected String nome; 
+    protected String email;
+
+    public Pessoa(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract void exibirInformacoes(); 
+}
